b075d26f127c2e971bf2f35de63472f1fe48b54f
Fujita_Kentaro <k021g1261@g.neec.ac.jp>
2023-01-11 14:01:57 +0900

    ESC to escape

--- main.cpp
+++ main.cpp
@@ -118,6 +118,9 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 		if (msg.message == WM_QUIT) {
 			break;
 		}
+		if (input->PushKey(DIK_ESCAPE)) {
+			break;
+		}
 		
 		fps->FpsControlBegin();
 
@@ -131,8 +134,25 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 		//入力の更新
 		input->Update();
 
-		object3d->Update();
+		//object3d->Update();
 		object3d_2->Update();
+
+		object3d->Update();
+		// オブジェクト移動
+		if (input->PushKey(DIK_UP) || input->PushKey(DIK_DOWN) || input->PushKey(DIK_RIGHT) || input->PushKey(DIK_LEFT))
+		{
+			// 現在の座標を取得
+			XMFLOAT3 position = object3d->GetPosition();
+
+			// 移動後の座標を計算
+			if (input->PushKey(DIK_UP)) { position.y += 1.0f; }
+			else if (input->PushKey(DIK_DOWN)) { position.y -= 1.0f; }
+			if (input->PushKey(DIK_RIGHT)) { position.x += 1.0f; }
+			else if (input->PushKey(DIK_LEFT)) { position.x -= 1.0f; }
+
+			// 座標の変更を反映
+			object3d->SetPosition(position);
+		}
 
 		if (CheckFlag == 0) {
 			//音声再生
